52b1f06d87b40d75d01183d191c38eb6bee61a9c
Валерія Козел <valeriakozel@MacBook-Air.local>
2026-01-24 01:14:29 +0200

    message

--- scrapy_scrapers/scrapy_scrapers/spiders/buduj24_pl.py
+++ scrapy_scrapers/scrapy_scrapers/spiders/buduj24_pl.py
@@ -11,34 +11,49 @@ class Buduj24PLSpider(scrapy.Spider):
         for link in product_links:
             yield response.follow(link, callback=self.parse_product)
 
-        next_page = response.xpath("a.next::attr(href)").extract_first()
-        if next_page is not None:
+        next_page = response.xpath("//a[contains(@class, 'next')]/@href").extract_first()
+        if next_page:
             yield response.follow(next_page, callback=self.parse)
 
     def parse_product(self, response):
 
         name = response.xpath("//h1/span/text()").extract_first()
-        mpn = response.xpath("//span[@class='mpn']/text()").extract_first()
-        ean = response.xpath("//span[@class='ean']/text()").extract_first()
-        price = response.xpath("//span[contains(@class, 'price')]/text()").extract_first()
-        stock = response.xpath("//span[contains(@class, 'availability')]/text()").extract_first()
-        description = response.xpath("//div[@class='product-description']//text()").extract_first()
-        average_rating = response.xpath("//*[contains(@class,'rating-value')]/text()").extract_first()
-        reviews_amount = response.xpath("//*[contains(@class,'review-count')]/text()").extract_first()
-
-        if self.brand.lower() not in name.lower():
-            return
+        if name:
+            name = name.strip()
 
         item = Product()
         item["url"] = response.url
         item["name"] = name
-        item["mpn"] = mpn
-        item["ean"] = ean
-        item["price"] = price
-        item["stock"] = stock
-        item["description"] = description
-        item["average_rating"] = average_rating
-        item["reviews_amount"] = reviews_amount
+
+        item["mpn"] = response.xpath("//span[@class='mpn']/text()").extract_first()
+        if item["mpn"]:
+            item["mpn"] = item["mpn"].strip()
+
+        item["ean"] = response.xpath("//span[@class='ean']/text()").extract_first()
+        if item["ean"]:
+            item["ean"] = item["ean"].strip()
+
+        item["price"] = response.xpath("//span[contains(@class, 'price')]/text()").extract_first()
+        if item["price"]:
+            item["price"] = item["price"].strip()
+
+        item["stock"] = response.xpath("//span[contains(@class, 'availability')]/text()").extract_first()
+        if item["stock"]:
+            item["stock"] = item["stock"].strip()
+
+        item["description"] = response.xpath("//div[@class='product-description']//text()").extract_first()
+
+        item["average_rating"] = response.xpath("//*[contains(@class,'rating-value')]/text()").extract_first()
+        if item["average_rating"]:
+            item["average_rating"] = item["average_rating"].strip()
+
+        item["reviews_amount"] = response.xpath("//*[contains(@class,'review-count')]/text()").extract_first()
+        if item["reviews_amount"]:
+            item["reviews_amount"] = item["reviews_amount"].strip()
+
         item["id"] = response.url.split("/") [-1]
+
+        if self.brand.lower() not in (item["name"] or "").lower():
+            return
 
         yield item
