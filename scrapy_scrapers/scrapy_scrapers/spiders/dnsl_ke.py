3146e7e8562400088813d26161a1c26339f52220
Валерія Козел <valeriakozel@MacBook-Air.local>
2026-02-03 12:41:37 +0200

    fix

--- scrapy_scrapers/scrapy_scrapers/spiders/dnsl_ke.py
+++ scrapy_scrapers/scrapy_scrapers/spiders/dnsl_ke.py
@@ -3,20 +3,21 @@ from scrapy_scrapers.items import Product
 
 class DnslKeSpider(scrapy.Spider):
     name = "dnsl_ke"
+    allowed_domains = ["dnsl.co.ke"]
     start_urls = ["https://dnsl.co.ke/?s=Epson&post_type=product&product_cat=0"]
     brand = "Epson"
 
     def parse(self, response):
-        product_links = response.xpath("//h2[@class='woocommerce-loop-product__title']/a/@href").extract()
+        product_links = response.xpath("//a[contains(@class, 'woocommerce-loop-product__link')]/@href").extract()
         for link in product_links:
             yield response.follow(link, callback=self.parse_product)
 
-        next_page = response.xpath("//a[@class='next page-numbers']/@href").extract_first()
+        next_page = response.xpath("//a[@class='next page-numbers']/@href").get()
         if next_page:
             yield response.follow(next_page, callback=self.parse)
 
     def parse_product(self, response):
-        name = self.cleanup_string(response.xpath("//h1/text()").extract_first())
+        name = self.clean(response.xpath("//h1/text()").get())
 
         if not name or self.brand.lower() not in name.lower():
             return
@@ -24,19 +25,26 @@ class DnslKeSpider(scrapy.Spider):
         item = Product()
         item["url"] = response.url
         item["name"] = name
-
-        item["mpn"] = self.cleanup_string(response.xpath("//span[@class='sku']/text()").extract_first())
+        item["mpn"] = self.cleanup_string(response.xpath("//span[@class='sku']/text()").get())
         item["ean"] = None
-        item["price"] = self.cleanup_string(response.xpath("//p[@class='price']//text()").extract_first())
+        item["price"] = self.cleanup_string("".join(response.xpath("//p[@class='price']//text()").getall()))
         item["stock"] = None
-        item["description"] = self.cleanup_string(response.xpath("//div[@id='tab-description']//text()").extract_first())
-        item["average_rating"] = self.clean_string(response.xpath("//div[contains(@class,'star-rating')]/@aria-label").extract_first())
+        item["description"] = self.cleanup_string(
+            " ".join(response.xpath("//div[@id='tab-description']//text()").getall()))
+        item["average_rating"] = self.cleanup_string(
+            response.xpath("//div[contains(@class,'star-rating')]/@aria-label").get())
         item["reviews_amount"] = None
-        item["id"] = response.url.split("/")[-1]
+        item["id"] = response.url.rstrip("/").split("/")[-1]
+
         yield item
 
-@staticmethod
-def cleanup_string(value):
-    if value:
-        return value.strip()
-    return None
+    def clean(self, value):
+        if not value:
+            return None
+        return " ".join(value.split()).strip()
+
+    @staticmethod
+    def cleanup_string(value):
+        if value:
+            return value.strip()
+        return None
